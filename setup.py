af909c5e30520180d5599c586471fd1b70b2dade
Gord Stephen <gord.stephen@nrel.gov>
2018-05-28 18:38:23 -0600

    Debug Julia calls

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='plexos2pras',
       version='0.1.0',
       packages=['plexos2pras'],
+      package_data={'plexos2pras': ['*.jl']},
       entry_points={
           'console_scripts': [
               'process-workbook = plexos2pras.process_workbook:_process_workbook',
