5f207505945d306dba6211df04b7c2e6d3f7caf7
Gord Stephen <gord.stephen@nrel.gov>
2018-09-21 17:42:04 -0600

    Make process-solutions command available again

--- peregrine_install.sh
+++ peregrine_install.sh
@@ -8,6 +8,19 @@ module load conda
 conda env update -f plexos2pras_env.yml
 source activate plexos2pras
 
+# Add process-solutions alias when entering environment
+pythoncode='
+import os; import inspect; import plexos2pras
+print(os.path.dirname(inspect.getfile(plexos2pras)))'
+scriptpath=$(python -c "$pythoncode")
+mkdir -p $CONDA_PREFIX/etc/conda/activate.d
+echo "alias process-solutions 'julia -p auto "$scriptpath"/process_solutions.jl'" > \
+    $CONDA_PREFIX/etc/conda/activate.d/set_alias.sh
+
+# Remove process-solutions alias when leaving environment
+mkdir -p $CONDA_PREFIX/etc/conda/deactivate.d
+echo "unalias process-solutions" > $CONDA_PREFIX/etc/conda/deactivate.d/unset_alias.sh
+
 # Set up custom PRAS dependencies
 julia_pkg_dir=$(julia -e 'println(Pkg.dir())')
 julia -e 'Pkg.add("Distributions"); Pkg.add("LightGraphs")'
--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(name='plexos2pras',
       entry_points={
           'console_scripts': [
               'process-workbook = plexos2pras.process_workbook:_process_workbook',
-              'process-solutions = plexos2pras.process_solutions:_process_solutions',
           ]
       },
       )
